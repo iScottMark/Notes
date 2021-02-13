41dcac402117ae5f61d67fa194be8a4f197011c2
ScottMark <xiaoma_hebut@163.com>
2021-02-13 18:28:09 +0800

    deploy gitee pages

--- deploy.sh
+++ deploy.sh
@@ -21,5 +21,6 @@ git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:iScottMark/Notes.git master:gh-pages
+git push -f git@gitee.com:iScottMark/Notes.git master:gh-pages
 
 cd -
